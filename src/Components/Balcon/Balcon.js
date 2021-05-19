bcd7a19924643152af903e5855d6f5531576df20
Mihai Dornescu <dornescu@gmail.com>
2021-05-19 16:06:40 +0300

    filter data

--- src/Components/Balcon/Balcon.js
+++ src/Components/Balcon/Balcon.js
@@ -1,4 +1,11 @@
+import ContentWrapper from "../UI/Helpers/ContentWrapper/ContentWrapper";
+import BalconInfo from "./BalconInfo";
+import BalconCard from "./BalconCard";
+
 const Balcon =()=>{
-    return <h1>Balcon</h1>
+    return <ContentWrapper>
+        <BalconInfo />
+        <BalconCard />
+    </ContentWrapper>
 };
 export default Balcon;
---
+++ src/Components/Balcon/BalconCard.js
@@ -0,0 +1,40 @@
+import dataInfo from '../../data.json'
+import {Button, Card, CardGroup} from "react-bootstrap";
+import CardHelper from "../UI/Helpers/CardHelper/CardHelper";
+import {useState} from "react";
+
+
+
+const BalconCard =()=>{
+    const [data, setData]=useState(dataInfo)
+    const filteredData = data.filter((item)=>{
+        return item.name ==="Baie"
+    })
+
+    let title ='dsadasdas';
+    console.log(data)
+    // return <CardHelper title={title}>
+    //     {data.map((item)=>{
+    //         console.log(item)
+    //     })}
+    // </CardHelper>
+
+    return <CardGroup className="home-card">
+        {filteredData.map((item) => {
+            const {id, img, title, desc} = item;
+            return <div className="col-lg-6 col-sm-4 py-1"  key={id}>
+                <Card className="home-card-item">
+                    <Card.Img variant="top"
+                              src={img} className='img-general'/>
+                    <Card.Body>
+                        <Card.Title>{title}</Card.Title>
+                        <Card.Text>
+                            {desc}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+        })}
+    </CardGroup>
+}
+export default BalconCard;

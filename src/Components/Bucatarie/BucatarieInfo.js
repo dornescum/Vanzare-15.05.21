5b586e960d1cab0c8c8acaf3a53f846909bd2f05
Mihai Dornescu <dornescu@gmail.com>
2021-05-20 20:08:55 +0300

    o [arte din info

--- src/Components/Bucatarie/BucatarieInfo.js
+++ src/Components/Bucatarie/BucatarieInfo.js
@@ -2,10 +2,10 @@ import InfoCard from "../UI/Helpers/InfoCard/InfoCard";
 
 const BucatarieInfo =()=>{
     let title = 'Bucatarie'
-    let desc = 'orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    let desc = 'Mobilata, complet echipata: frigider incapator cu congelator separat, cupotor cu microunde, plita pe gaz si cutor electric, baterie Hansgrohe si sistem de filtrare a apei.\n'
     return <InfoCard>
-        <h1>{title}</h1>
-        <p>{desc}</p>
+        <h1 >{title}</h1>
+        <p style={{letterSpacing:"1.2px"}}>{desc}</p>
     </InfoCard>
 }
 export default BucatarieInfo;
--- src/Components/Parcare/ParcareInfo.js
+++ src/Components/Parcare/ParcareInfo.js
@@ -4,7 +4,7 @@ import InfoCard from "../UI/Helpers/InfoCard/InfoCard";
 const ParcareInfo =()=>{
 
     let title = 'Parcare'
-    let desc = 'orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    let desc ='Situat langa scara blocului, echipat cu sistem de blocare al parcarii. In situatia in care nu aveti masina se poate inchiria lejer, deoarece locurile de parcare sunt la foarte mare cautare in zona.'
     return <InfoCard>
         <h1>{title}</h1>
         <p>{desc}</p>
